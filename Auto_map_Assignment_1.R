699e856124cbbe51615db61917fe1c155fa16507
unknown <boeij004@D0105083.wurnet.nl>
2013-11-07 14:28:54 +0100

    first try

---
+++ Auto_map_Assignment_1.R
@@ -0,0 +1,24 @@
+# Author: Niene Boeijen
+# November 2013
+# requires rasta library
+# Quick description of what the function does:
+  ## Generates a map outomatically when giving country, and level. 
+
+library(rasta)
+path = "M:/My Documents/R_Lesson_1"   ## define path
+map <- function(country, x){
+  
+  map <- raster::getData("GADM",country = country, level = x, 
+                         download = TRUE, path = path)
+  plot(map,bg = "dodgerblue", axes=T)
+  plot(map, lwd = 15, border = "skyblue", add=T)
+  plot(map, col = "green4", add = T)
+  
+  box(lwd = 5, col = "green4")
+  mtext(side = 3, line = 1, paste("Nice map of", country), cex = 2)
+  mtext(side = 1, "Longitude", line = 2.5, cex=1.1)
+  mtext(side = 2, "Latitude", line = 2.5, cex=1.1)
+  text(122.08, 13.22, "Projection: Geographic Coordinate System: WGS 1984 Data Source: GADM.org", adj = c(0, 0), cex = 0.7, col = "grey20")
+}
+
+map("Belgium",1)
---
+++ Leap_year_Assignment_2_Niene_Boeijen.R
@@ -0,0 +1,33 @@
+# Author: Niene Boeijen
+# November 2013
+# requires
+# year = numeric
+# Quick description of what the function does. 
+
+is.leap <- function(year) {
+  if(is.numeric(year)) {
+    if(year > 1582) {
+      out <- ifelse(year%%100 == 0, year%%400 == 0, year%%4 == 0) 
+    }
+    else if(year <= 1582) {
+      out <- paste(year," is out of valid range")
+    }
+  }
+  else if(is.character(year)) {
+    stop("Object of class numeric expected!")
+  }
+  return(out)
+}
+
+is.leap(2000)
+is.leap(2002)
+is.leap("niene")
+is.leap(1582)
+is.leap(1996)
+is.leap(1652)
+is.leap(1999)
+is.leap(1998)
+is.leap(2)
+
+
+
